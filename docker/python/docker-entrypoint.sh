1aa5f72244c652a62171d3e302505a4f9f5f1003
Vaibhav G <gvaibhav1734@gmail.com>
2019-03-24 10:37:11 +0530

    Python bug fix.

--- docker/python/docker-entrypoint.sh
+++ docker/python/docker-entrypoint.sh
@@ -9,8 +9,8 @@ mv code code.py
 
 # TODO: Restrict container network access
 
-for testcase in $(ls testcases); do
-    python3 ./submission/code.py < testcases/$testcase \
-        1>./submission/output/$testcase \
-        2>./submission/error/$testcase
+for testcase in $(ls $testcases_directory); do
+    python3 code.py 0<"$testcases_directory/$testcase" \
+        1>"$submission_directory/output/$testcase" \
+        2>"$submission_directory/error/$testcase"
 done
